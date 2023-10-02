21bbd49a96318d05df98f5993bfbd9f5cbf67254
swlws <979623074@qq.com>
2023-10-02 10:28:59 +0800

    code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # WeLink
 
-A Tool For Recording Web Site.
+A Tool For Recording Web Site. All Data Is Only Stored Locally.
 
 [在线站点](https://swlws.github.io/WeLink/)
--- projects/ui-pc/src/pages/dashboard/index.tsx
+++ projects/ui-pc/src/pages/dashboard/index.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <article className={styles.container}>
       <header>Welcome To WeLink</header>
       <main>A Tool For Recording Web Site.</main>
+      <br />
       <main>All Data Is Only Stored Locally.</main>
     </article>
   );
